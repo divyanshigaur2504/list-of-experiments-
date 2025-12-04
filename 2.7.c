df52f9a9a1708a3d407835d95306acb5556ac6e7
divyanshigaur2504 <divyanshigaur108@gmail.com>
2025-12-04 17:44:10 +0530

    Create 2.7.c

---
+++ 2.7.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    (n % 2 == 0) ? printf("%d is Even\n", n) : printf("%d is Odd\n", n);
+    return 0;
+}
