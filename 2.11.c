df0cb5fde90bba65778fef0d43f44ecaa8022183
divyanshigaur2504 <divyanshigaur108@gmail.com>
2025-12-04 17:51:27 +0530

    Create 2.11.c

---
+++ 2.11.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    char op;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("Enter operator (+, -, *, /, %%): ");
+    scanf(" %c", &op);
+
+    switch(op) {
+        case '+': printf("Result = %d\n", a + b); break;
+        case '-': printf("Result = %d\n", a - b); break;
+        case '*': printf("Result = %d\n", a * b); break;
+        case '/': printf("Result = %d\n", a / b); break;
+        case '%': printf("Result = %d\n", a % b); break;
+        default: printf("Invalid operator\n");
+    }
+    return 0;
+}
