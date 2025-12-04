d5ead00e61b3775022131b3d5dbe67b01ed994d7
divyanshigaur2504 <divyanshigaur108@gmail.com>
2025-12-04 17:39:13 +0530

    Add book details input and display program

---
+++ 6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    char title[20], author[20], publisher[20];
+    int isbn, year;
+    float price;
+
+    printf("Enter Book Title: ");
+    scanf("%s", title);
+    printf("Enter Author Name: ");
+    scanf("%s", author);
+    printf("Enter ISBN: ");
+    scanf("%d", &isbn);
+    printf("Enter Price: ");
+    scanf("%f", &price);
+    printf("Enter Publisher: ");
+    scanf("%s", publisher);
+    printf("Enter Year of Publication: ");
+    scanf("%d", &year);
+
+    printf("\n--- Book Details ---\n");
+    printf("Title: %s\nAuthor: %s\nISBN: %d\nPrice: %.2f\nPublisher: %s\nYear: %d\n",
+           title, author, isbn, price, publisher, year);
+    return 0;
+}
