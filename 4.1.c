bf4f663981a3d3ca62e6982ec26381f4f6450d94
divyanshigaur2504 <divyanshigaur108@gmail.com>
2025-12-04 18:05:08 +0530

    Create 4.1.c

---
+++ 4.1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    int n, i;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+
+    printf("Enter %d integers: ", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int largest = arr[0], second = -99999;
+    for(i = 1; i < n; i++) {
+        if(arr[i] > largest) {
+            second = largest;
+            largest = arr[i];
+        } else if(arr[i] > second && arr[i] != largest) {
+            second = arr[i];
+        }
+    }
+
+    printf("Second largest element = %d\n", second);
+    return 0;
+}
