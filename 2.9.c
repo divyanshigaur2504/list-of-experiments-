c907b7b32c0cca9561d19b2fb0dc930d191f4782
divyanshigaur2504 <divyanshigaur108@gmail.com>
2025-12-04 17:46:10 +0530

    Create 2.9.c

---
+++ 2.9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    int num, sum = 0, temp, digit;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    temp = num;
+
+    while(temp != 0) {
+        digit = temp % 10;
+        sum += pow(digit, 3);
+        temp /= 10;
+    }
+
+    if(sum == num)
+        printf("%d is an Armstrong number\n", num);
+    else
+        printf("%d is not an Armstrong number\n", num);
+
+    return 0;
+}
