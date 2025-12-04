ca46efe0c4ad1dca074ff2999b4859225eb27458
divyanshigaur2504 <divyanshigaur108@gmail.com>
2025-12-04 18:23:10 +0530

    Create 6.3.c

---
+++ 6.3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+struct Student {
+    int roll;
+    char name[20];
+    float marks;
+};
+
+int main() {
+    int n;
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    struct Student s[n];
+    for(int i=0; i<n; i++) {
+        printf("Enter Roll, Name, Marks for student %d: ", i+1);
+        scanf("%d %s %f", &s[i].roll, s[i].name, &s[i].marks);
+    }
+
+    printf("\n--- Student Records ---\n");
+    for(int i=0; i<n; i++) {
+        printf("Roll: %d, Name: %s, Marks: %.2f\n", s[i].roll, s[i].name, s[i].marks);
+    }
+    return 0;
+}
