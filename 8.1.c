fb219de7b72122c32ac34eda766bfe7544109247
divyanshigaur2504 <divyanshigaur108@gmail.com>
2025-12-05 17:50:51 +0530

    Create 8.1.c

---
+++ 8.1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n, i, sum=0, max, min;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int *arr = (int*) malloc(n * sizeof(int));
+
+    printf("Enter %d integers: ", n);
+    for(i=0; i<n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    max = min = arr[0];
+    for(i=1; i<n; i++) {
+        if(arr[i] > max) max = arr[i];
+        if(arr[i] < min) min = arr[i];
+    }
+
+    printf("Sum = %d\nMax = %d\nMin = %d\n", sum, max, min);
+
+    free(arr);
+    return 0;
+}
